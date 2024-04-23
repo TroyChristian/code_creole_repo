57f4f184125fe42334ca4934d08b6b7fcec61e7e
Nkisi <nkisi@pop-os.localdomain>
2024-04-23 19:47:10 -0400

    add usa_flag static icon. Show current_languages flag has watermark in article_detail_view

--- code_creole/articles/handlers.py
+++ code_creole/articles/handlers.py
@@ -1,6 +1,8 @@
 from django.utils.translation import get_language 
 def format_content(content):
     # Simple Python function to replace markdown code blocks with HTML
-    content = content.replace("```python", "<pre class='code-block'><code>")
-    content = content.replace("```", "</code></pre>")
+    content = content.replace("<code-block>", "<pre class='code-block'><code>")
+    content = content.replace("</code-block>", "</code></pre>")
+    content = content.replace("<inline-code>", "<span class='inline-code'>")
+    content = content.replace("</inline-code>", "</span>")
     return content
