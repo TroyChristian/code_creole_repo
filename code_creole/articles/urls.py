188921ab08d4e80e2ff184472c12055dd06cd291
Nkisi <nkisi@pop-os.localdomain>
2024-04-24 17:42:40 -0400

    Add navbar_footer template, extend home and article detail with it. Add language toggle returns to page toggled from.

--- code_creole/articles/urls.py
+++ code_creole/articles/urls.py
@@ -2,7 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+#articles app core
     path('', views.home, name='home'), 
     path('article/<int:article_id>', views.article_detail_view, name='article_detail'),
-    path('set-language/', views.set_language, name='set-language'),
+    path('set-language/', views.set_language, name='set-language'), 
+
+    #registration
+    path('sign_up', views.sign_up, name='sign_up'),
 ]
